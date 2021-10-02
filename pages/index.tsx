312d74238b2c8c32fe201c5b16f89f65a2558332
fooi <fooi@fmac.local>
2021-10-02 23:38:18 +0900

    ひっくり返り実装

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
@@ -24,13 +25,54 @@ const Cell = styled.div`
   border: 1px solid black;
 `
 
+const Blackstone = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  margin: 4px 4px;
+  background-color: black;
+  border-radius: 50%;
+`
+const Whitestone = styled.div`
+  flex-direction: column;
+  width: 50px;
+  height: 50px;
+  margin: 4px 4px;
+  background-color: white;
+  border: 2px solid black;
+  border-radius: 50%;
+`
+
 const Home: NextPage = () => {
+  // prettier-ignore
+  const [board, setBoard] = useState([
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,1,2,0,0,0],
+    [0,0,0,2,1,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+  ])
+  const [currentColor, setCurentcolor] = useState(1)
+  const onClick = (x: number, y: number) => {
+    console.log(x, y)
+    const newBoard: number[][] = JSON.parse(JSON.stringify(board))
+    newBoard[y][x] = currentColor
+    setBoard(newBoard)
+    setCurentcolor(3 - currentColor)
+  }
   return (
     <Container>
       <Squarebox>
-        {[...Array(64)].map((_, i) => (
-          <Cell key={i}></Cell>
-        ))}
+        {[...board].map((row, y) =>
+          row.map((color, x) => (
+            <Cell key={x + y * 8} onClick={() => onClick(x, y)}>
+              {color !== 0 && (color === 1 ? <Blackstone /> : <Whitestone />)}
+            </Cell>
+          ))
+        )}
       </Squarebox>
     </Container>
   )
