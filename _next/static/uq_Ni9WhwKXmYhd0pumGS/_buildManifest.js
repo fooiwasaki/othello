6901d9117f345c1735d66255c9f6d45c7aee5495
fooiwasaki <fooiwasaki@users.noreply.github.com>
2021-10-02 11:32:47 +0000

    deploy: 7cb5ba049796700abd06fe28550062f1dc66786b

--- _next/static/uq_Ni9WhwKXmYhd0pumGS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/393-887205f6b00ce0c792c4.js","static/chunks/pages/index-38b7298108401756b907.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
